e443766ff6802de6820251b64949bf50c88317c9
SAYANGHOSH2002 <91898673+SAYANGHOSH2002@users.noreply.github.com>
2021-10-05 21:58:25 +0530

    pattern programming in python

---
+++ pattern.py
@@ -0,0 +1,12 @@
+#pattern programming in python
+for i in range(0,5):
+        for j in range(0,i+1):
+                print("*",end='')
+        print("\n")
+       
+#it will print this---
+*
+* *
+* * *
+* * * *
+* * * * *
